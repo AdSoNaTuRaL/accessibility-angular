31c0d53f40716ee73719fd3f45bc8a754089e567
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-22 12:23:36 +0200

    import directives

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
@@ -1,15 +1,12 @@
+import { KeyboardManagerModule } from './../../directives/keyboard-manager/keyboard-manager.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { YesNoButtonGroupComponent } from './yes-no-button-group.component';
 
-
-
 @NgModule({
   declarations: [YesNoButtonGroupComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [YesNoButtonGroupComponent]
+  imports: [CommonModule, KeyboardManagerModule],
+  exports: [YesNoButtonGroupComponent],
 })
-export class YesNoButtonGroupModule { }
+export class YesNoButtonGroupModule {}
