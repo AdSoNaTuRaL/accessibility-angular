2195a7f61fe1096881c47953a5172164003d5d30
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-26 17:48:28 +0200

    import animations and reactive forms

--- modal-acessibility/src/app/app.module.ts
+++ modal-acessibility/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { ModalModule } from './shared/components/modal/modal.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ModalModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
