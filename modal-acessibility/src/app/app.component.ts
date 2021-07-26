8cb2b71319400860e0fc1f0989e7d9e80a2b11d9
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-26 13:58:10 +0200

    refactor

--- modal-acessibility/src/app/app.component.ts
+++ modal-acessibility/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  ModalService,
+  ModalRef,
+} from './shared/components/modal/modal.service';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'a11y-p2';
+  @ViewChild('modal') modalTemplateRef: TemplateRef<any>;
+
+  firstName = 'Adson';
+  modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  show(): void {
+    this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User details',
+    });
+  }
 }
--- modal-acessibility/src/app/app.module.ts
+++ modal-acessibility/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ModalModule } from './shared/components/modal/modal.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,14 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ModalModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
